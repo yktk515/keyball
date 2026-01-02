083e5bb46a4c6ea3bb34c8b2a2c421435cc5d535
yktk515 <yktk20130727@gmail.com>
2026-01-02 02:40:36 +0000

    fix: コンボキー追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yktk/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yktk/keymap.c
@@ -29,38 +29,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_A     , KC_O     , KC_E     , KC_I     , KC_U     ,                            KC_G     , KC_T     , KC_K     , KC_S     ,LT(4,KC_N),
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_F     ,                            KC_B     , KC_H     , KC_J     , KC_L     , KC_SLSH  ,
     KC_TAB   , KC_LALT  , KC_LCTL  ,LSFT_T(KC_LNG2),LT(4,KC_SPC),LT(2,KC_LNG1),LT(5,KC_BSPC),LT(3,KC_ENT),KC_RSFT,KC_RCTL,KC_RALT,KC_LGUI
-  ),
+  )
   // qwer base layer
-  [1] = LAYOUT_universal(
+  ,[1] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
     KC_TAB   , KC_LALT  , KC_LCTL  ,LSFT_T(KC_LNG2),LT(4,KC_SPC),LT(2,KC_LNG1),LT(5,KC_BSPC),LT(3,KC_ENT),KC_RSFT,KC_RCTL,KC_RALT,KC_LGUI
   )
   // number/fn/move
-  [2] = LAYOUT_universal(
+  ,[2] = LAYOUT_universal(
     KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                            KC_6     , KC_7     , KC_8     , KC_9     , KC_0     ,
     KC_F2    , KC_F3    , KC_PGUP  , KC_F4    , KC_F5    ,                            KC_F8    , KC_F8    , KC_UP    , KC_F9    , KC_F10   ,
     KC_F1    , KC_HOME  , KC_PGDN  , KC_END   , KC_F6    ,                            KC_F7    , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_F11   ,
     KC_ESC   , _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , KC_F12
-  ),
+  )
   // symbol
-  [3] = LAYOUT_universal(
+  ,[3] = LAYOUT_universal(
     KC_ESC   , S(KC_1)  , S(KC_3)  , S(KC_4)  , KC_RBRC  ,                            KC_BSLS  , S(KC_5)  , S(KC_6)  , KC_EQL   , KC_DEL   ,
     S(KC_EQL),S(KC_LBRC), S(KC_7)  , S(KC_2)  , S(KC_8)  ,                            S(KC_9)  , KC_LBRC  ,S(KC_INT1), KC_QUOT  , KC_SCLN  ,
    S(KC_SCLN),S(KC_QUOT),S(KC_MINS),S(KC_COMM),S(KC_RCBR),                           S(KC_BSLS),S(KC_DOT) ,S(KC_INT3), KC_INT1  ,S(KC_SLSH),
     KC_TAB   , _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , _______
-  ),
-
+  )
   // mouse
-  [4] = LAYOUT_universal(
+  ,[4] = LAYOUT_universal(
     _______  , KC_PSCR  , KC_SCRL  , KC_PAUS  , _______  ,                            _______  , _______  , KC_BTN3  , _______  , _______  ,
     KC_CAPS  , KC_INS   , _______  , _______  , _______  ,                            _______  , KC_BTN1  , KC_PGUP  , KC_BTN2  , _______  ,
     _______  , _______  , _______  , _______  , _______  ,                            _______  , MO(5)    , KC_PGDN  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , _______
-  ),
+  )
   // extra custom
-  [5] = LAYOUT_universal(
+  ,[5] = LAYOUT_universal(
     RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , CM_TOGG  ,                            CM_OFF   , CM_ON    , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
